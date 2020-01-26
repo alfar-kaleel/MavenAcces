d4f7ef0e1dfffbd9fb53a243ee8daf313f73d802
unknown <alfarkaleel@gmail.com>
2020-01-26 14:14:45 +0530

    second commit

---
+++ mavenpro1/src/main/java/com/alfar/mavenpro1/Maven1class.java
@@ -0,0 +1,17 @@
+package com.alfar.mavenpro1;
+
+/**
+ * Hello world!
+ *
+ */
+public class Maven1class 
+{
+   
+    
+    
+    public void call() {
+    	
+    	System.out.println("Hello welcome to maven");
+    	
+    }
+}
---
+++ mavenpro2/src/main/java/com/alfar/mavenpro2/Maven2class.java
@@ -0,0 +1,23 @@
+package com.alfar.mavenpro2;
+
+import com.alfar.mavenpro1.Maven1class;
+
+/**
+ * Hello world!
+ *
+ */
+public class Maven2class
+{
+    public static void main( String [] args )
+    {
+       
+    	
+  Maven1class obj = new Maven1class();
+  
+  obj.call();
+    	
+    	
+    	
+    	
+    }
+}
